c9eaa1eca35873c4ed85da2d33f047dced792dd8
MohammedNabil1324 <MM3483@live.mdx.ac.uk>
2021-11-18 16:50:04 +0400

    Vue Setup

--- JS/lesson.js
+++ JS/lesson.js
@@ -1,4 +1,13 @@
 let app = new Vue({
     el: '#app',
-    
+    data: {
+        sitename: "Lessons",
+        lessons: {
+            id:01,
+            subject:"English",
+            location:London,
+            price:1500,
+            space:5
+        }
+    }
 })
