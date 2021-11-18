64e99423101f4a8937aef9907a8bcffd8076f299
MohammedNabil1324 <MM3483@live.mdx.ac.uk>
2021-11-18 17:13:31 +0400

    Lessons Setup

--- JS/lesson.js
+++ JS/lesson.js
@@ -1,13 +1,88 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        sitename: "Lessons",
-        lessons: {
-            id:01,
-            subject:"English",
-            location:London,
-            price:1500,
-            space:5
-        }
-    }
-})
+  el: "#app",
+  data: {
+    sitename: "Lessons",
+    lessons: [
+      {
+        id: 01,
+        subject: "English",
+        location: London,
+        price: 1500,
+        space: 5,
+      },
+
+      {
+        id: 02,
+        subject: "",
+        location:"York",
+        price: 1500,
+        space: 5,
+      },
+
+      {
+        id: 03,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 04,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 05,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 06,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 07,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 08,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 09,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+      
+      {
+        id: 10,
+        subject: "",
+        location: "",
+        price: 1500,
+        space: 5,
+      },
+
+    ],
+  },
+});
